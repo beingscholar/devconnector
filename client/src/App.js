211bd2679f4aa4f3cc17c118027adf406eb1b5aa
Mayank Agarwal <mayank.agarwal@lexxdigital.com>
2019-06-04 04:16:08 +0530

    Prepare for deployment

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import NotFound from './components/layout/NotFound';
 
 import PrivateRoute from './components/routing/PrivateRoute';
 
@@ -67,6 +68,7 @@ const App = () => {
                 path="/add-education"
                 component={AddEducation}
               />
+              <Route component={NotFound} />
             </Switch>
           </section>
         </Fragment>
